9226386599a2aaf618cd2c7bbbc0e7a38206689d
SON NGUYEN <myantj3m@gmail.com>
2018-11-27 15:31:56 +0700

    commit 2

---
+++ src/main/java/com/codegym/configuration/SecurityConfiguration.java
@@ -0,0 +1,37 @@
+package com.codegym.configuration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import java.net.Authenticator;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    CustomSuccessHandler customSuccessHandler;
+
+    @Autowired
+    public void configureGlobalSecurity(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("son").password("111111").roles("USER");
+        auth.inMemoryAuthentication().withUser("admin").password("111111").roles("ADMIN");
+        auth.inMemoryAuthentication().withUser("dat09").password("111111").roles("ADMIN", "COSUNG");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/", "/home").access("hasRole('USER')")
+                .antMatchers("/admin/**").access("hasRole('ADMIN')")
+                .antMatchers("/co_sung/**").access("hasRole('ADMIN') and hasRole('COSUNG')")
+                .and().formLogin().successHandler(customSuccessHandler)
+                .usernameParameter("ssoId").passwordParameter("password")
+                .and().csrf()
+                .and().exceptionHandling().accessDeniedPage("/Access_Denied");
+    }
+}
